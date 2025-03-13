3b249a29d038a6b08697091eecb6d7319bad7a41
mariaripoll4 <mrg201@alu.ua.es>
2025-03-13 20:10:33 +0100

    Create servicio.py
    Crear versión inicial de la clase Servicio

---
+++ servicio.py
@@ -0,0 +1,13 @@
+class Servicio:
+    """
+    Los precios serán listas dnd indico el precio de cada articulo
+    Añadir personal
+    """
+
+    def __init__(self, nombre, descripcion, precio, horario):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio = precio
+        self.horario = horario
+
+
