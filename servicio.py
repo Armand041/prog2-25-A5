7dbb5f59363fdbd56cd453e25945b67f2e8c29ac
mariaripoll4 <mrg201@alu.ua.es>
2025-04-10 18:12:32 +0200

    Arreglar integracion Staff con Servicio

--- servicio.py
+++ servicio.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 
 from Personal.staff import Staff
-#from Personal.staff import Staf
 
 
 """
@@ -32,32 +31,31 @@ class Servicio(ABC):
         self.horario = horario
         self.alquiler = alquiler
         self.lugar = lugar
-        # TODO: Cuando juntemos la clase Staff, utilizarla aqui
         self.trabajadores = trabajadores
+
 
     def contratar_trabajador(self, trabajador):
         """
-        Método que añade trabajadores a la lista Trabjadores si queremos contratar a alguien
+        Método que añade trabajadores a la lista Trabajadores si queremos contratar a alguien
         """
         trabajador.cambiar_puesto(self.nombre)
 
         self.trabajadores.append(trabajador)
 
-    def despedir_trabajadores(self, trabajador):
+ #   def despedir_trabajadores(self, trabajador):
+    def despedir_trabajadores(self, dni_trabajador: str):
+
         """
-        Método que elimina los trabajadrores de la lista Trabajadores en el caso en el que queramos despedir a alguien
+        Método que elimina los trabajadores de la lista Trabajadores en el caso en el que queramos despedir a alguien
         """
+
         for trabajador in self.trabajadores:
             if trabajador.dni == dni_trabajador:
-                trabajador.cambiar_puesto("")  # Se vacía el puesto de trabajo
-                self.trabajadores.remove(trabajador)  # Elimina al trabajador de la lista
-                print(f"El trabajador con DNI {dni_trabajador} ha sido despedido y su puesto ha sido eliminado.")
-                return  # Terminamos la función después de despedir al trabajador
-
-            # Si no se encuentra el trabajador con ese DNI
+                trabajador.cambiar_puesto("")
+                self.trabajadores.remove(trabajador)
+                print(f"El trabajador con DNI {dni_trabajador} ha sido despedido.")
+                return
         print(f"No se encontró al trabajador con el DNI {dni_trabajador}.")
-
-
 
     @abstractmethod
     def obtener_informacion(self):
@@ -68,8 +66,8 @@ class Servicio(ABC):
 
 
 class Comida(Servicio):
-    def __init__(self, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
-        super().__init__(horario, alquiler, lugar, trabajadores)
+    def __init__(self, nombre: str, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
+        super().__init__(nombre, horario, alquiler, lugar, trabajadores)
 
     def obtener_informacion(self):
         menu_comida = {'Hamburguesa ': 12, 'Perrito caliente': 8, '1 porción de pizza grande': 5, 'Patatas fritas': 5, 'Churros': 7, 'Helado': 3, 'Fruta con chocolate': 6}
@@ -77,44 +75,40 @@ class Comida(Servicio):
 
 
 class Bebida(Servicio):
-    def __init__(self, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
-        super().__init__(horario, alquiler, lugar, trabajadores)
+    def __init__(self, nombre: str, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
+        super().__init__(nombre, horario, alquiler, lugar, trabajadores)
 
     def obtener_informacion(self):
         bebidas_sin_alcohol = {}
         bebidas_con_alcohol = {}
         return f'Este puesto de bebidas ofrece bebidas con alcohol: {bebidas_con_alcohol} y sin alcohol: {bebidas_sin_alcohol}'
 
-class Tatto(Servicio):
-    def __init__(self, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
-        super().__init__(horario, alquiler, lugar, trabajadores)
+class Tattoo(Servicio):
+    def __init__(self, nombre: str, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
+        super().__init__(nombre, horario, alquiler, lugar, trabajadores)
 
     def obtener_informacion(self):
-        tatto = {'Pequeño': 50, 'Mediano': 120, 'Grande': 300, 'Muy grande': 750}
-        return f'Este puesto de tatuajes ofrece los siguientes precios dependiendo del tamaño: {tatto}'
+        tattoo = {'Pequeño': 50, 'Mediano': 120, 'Grande': 300, 'Muy grande': 750}
+        return f'Este puesto de tatuajes ofrece los siguientes precios dependiendo del tamaño: {tattoo}'
 
 class Merch(Servicio):
-    def __init__(self, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
-        super().__init__(horario, alquiler, lugar, trabajadores)
+    def __init__(self, nombre: str, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
+        super().__init__(nombre, horario, alquiler, lugar, trabajadores)
 
     def obtener_informacion(self):
         merch = {'Camiseta': 25, 'Sudaderas': 40, 'Taza': 10, 'Pulsera': 5, 'Termo': 20}
         return f'Este puesto de merchandising ofrece: {merch}'
 
 class SonidoLuces(Servicio):
-    def __init__(self, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
-        super().__init__(horario, alquiler, lugar, trabajadores)
+    def __init__(self, nombre: str, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
+        super().__init__(nombre, horario, alquiler, lugar, trabajadores)
 
     def obtener_informacion(self):
         return 'Este puesto no ofrece ningún prodcuto al público si no que consta de una serie de trabajadores que gestionan las luces y el sonido empleado en el festival controlandoq que todo funcione correctamente'
 
 class EntradasSeguridad(Servicio):
-    def __init__(self, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
-        super().__init__(horario, alquiler, lugar, trabajadores)
+    def __init__(self, nombre: str, horario: str, alquiler: float, lugar: str, trabajadores: list[str]):
+        super().__init__(nombre, horario, alquiler, lugar, trabajadores)
 
     def obtener_informacion(self):
         return 'Este puesto no ofrece ningún prodcuto al público si no que una serie de trabajadores se encargan de controlar las entradas del recinto'
-
-
-pepe = Staff('03-03-2000', '334535', )
-servicio1 = Comida('horario', 4,5, 'lugar', )
