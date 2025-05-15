e0a52583f15c892106bb15bf626a8f4ae16a46e1
PabloSerna4542 <pss81@alu.ua.es>
2025-05-15 18:23:55 +0200

    Actualización de main
    He corregido algunos errores de main y he implementado el csv de informacion festivales. Ahora, al inicio de la ejecución se lee el csv para que los festivales ya creados se tengan en cuenta. También he añadido control de excepciones antes de las llamadas que usan el token porque saltaba error al no estar la variable token inicializada. También he cambiado el bucle de la opción if then else por uno de tipo match case.  He cambiado la manera de pedir la fecha del festival para tener un formato consistente, habrá que hacer lo mismo con las funciones de cambiar esa fecha.  Y habrá alguna cosa creada en los ficheros de hacer pruebas,  por si quereis probarlo, la contraseña del usuario juan es juan123.

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ para manejar los distintos festivales y personas que trabajan en ellos y acuden 
 Permite que estos datos se guarden en ficheros para que tengan permanencia.
 """
 
+import csv
 import requests
+import calendar
 from festival import Festival
 from publico import Publico
 from Personal.artista import Artista
@@ -16,7 +18,19 @@ from Personal.staff import Staff
 from servicio import EntradasSeguridad, SonidoLuces, Merch, Tattoo, Bebida, Comida
 URL = 'http://127.0.0.1:5000/'
 festivales = []
-URL = 'http://127.0.0.1:5000/'
+try:
+    contador=0
+    with open('informacion_festivales.csv','r') as datos_festivales:
+        reader= csv.reader(datos_festivales)
+        for fila in reader:
+            if contador==0:
+                contador+=1
+            else:
+                festivales.append(Festival(fila[0],fila[1],fila[2],int(fila[3]),float(fila[4])))
+except FileNotFoundError:
+    print('No encontrado el archivo con los festivales para inicializarlos.')
+
+
 
 
 def seleccionar_festival():
@@ -76,171 +90,205 @@ while True:
         print('13. Terminar')
         opcion = input('Introduce una de las opciones (por número): ')
 
-
-    if opcion == '1':
-        usuario = input('Elige un nombre de usuario: ')
-        password = input('Elige una contraseña: ')
-        r = requests.post(f'{URL}/signup?user={usuario}&password={password}')
-        if r.status_code == 200:
-            print(f'Usuario {usuario} creado correctamente')
-        else:
-            print(f'Error al crear usuario: {r.status_code} - {r.text}')
-
-    elif opcion == '2':
-        usuario = input('Usuario: ')
-        password = input('Password: ')
-        r = requests.get(f'{URL}/signin?user={usuario}&password={password}')
-        if r.status_code == 200:
-            token = r.text
-            print('Inicio de sesión correcto')
-        else:
-            print('Error al iniciar sesión:', r.text)
-
-    elif opcion == '3':
-        usuario = input('Introduce el nombre de usuario a eliminar: ')
-        password = input('Introduce la contraseña del usuario: ')
-        r = requests.delete(f'{URL}/signout?user={usuario}&password={password}')
-        if r.status_code == 200:
-            print(f'Usuario {usuario} eliminado correctamente')
-        else:
-            print(f'Error al eliminar usuario: {r.status_code} - {r.text}')
-
-    elif opcion == '4':
-
-        nombre_fest = input('Introduce el nombre del festival a crear: ')
-        fecha_fest = input('Introduce la fecha del festival a crear: ')
-        lugar_fest = input('Introduce el lugar del festival: ')
-        aforo_fest = input('Introduce el aforo del festival a crear: ')
-        while not aforo_fest.isdigit():
-            print('Por favor, introduzca un número entero')
-            aforo_fest = input('Introduce el aforo del festival a crear: ')
-        aforo_fest = int(aforo_fest)
-        coste_fest = ''
-        while True:
-            try:
-                coste_fest = float(input('Introduce el coste del festival (en €): '))
-                break
-            except ValueError:
-                print('Por favor, introduzca un número válido')
-        r = requests.post(f'{URL}/data?nombre={nombre_fest}&fecha={fecha_fest}&lugar={lugar_fest}&aforo={aforo_fest}&coste={coste_fest}', headers={'Authorization': f'Bearer {token}'})
-        print(r.text + ' (' + str(r.status_code) + ')')
-
-        if r.status_code == 200:
-            nuevo_festival = Festival(nombre_fest, fecha_fest, lugar_fest, aforo_fest, coste_fest)
-            festivales.append(nuevo_festival)
-
-
-    elif opcion == '5':
-        if not festivales:
-            print("No hay festivales disponibles. Carga o crea alguno primero.")
-        else:
-            print('Elige un festival para eliminar:')
-            festival_a_eliminar = seleccionar_festival()
-            r = requests.delete(f'{URL}/data?festival={festival_a_eliminar.nombre}', headers={'Authorization': f'Bearer {token}'})
-            print(r.text + ' (' + str(r.status_code) + ')')
+    match opcion:
+        case '1':
+            usuario = input('Elige un nombre de usuario: ')
+            password = input('Elige una contraseña: ')
+            r = requests.post(f'{URL}/signup?user={usuario}&password={password}')
             if r.status_code == 200:
-                festivales.remove(festival_a_eliminar)
+                print(f'Usuario {usuario} creado correctamente. \n----------------------------------------')
+            else:
+                print(f'Error al crear usuario: {r.status_code} - {r.text}. \n----------------------------------------')
 
-    elif opcion == '6':
-        if len(festivales) == 0:
-            print('No hay festivales disponibles')
-        else:
+        case'2':
+            usuario = input('Usuario: ')
+            password = input('Password: ')
+            r = requests.get(f'{URL}/signin?user={usuario}&password={password}')
+            if r.status_code == 200:
+                token = r.text
+                print('Inicio de sesión correcto. \n----------------------------------------')
+            else:
+                print('Error al iniciar sesión:', r.text, '\n----------------------------------------')
+
+        case '3':
+            usuario = input('Introduce el nombre de usuario a eliminar: ')
+            password = input('Introduce la contraseña del usuario: ')
+            r = requests.delete(f'{URL}/signout?user={usuario}&password={password}')
+            if r.status_code == 200:
+                print(f'Usuario {usuario} eliminado correctamente. \n----------------------------------------')
+            else:
+                print(f'Error al eliminar usuario: {r.status_code} - {r.text}. \n----------------------------------------')
+
+        case'4':
+            nombre_fest = input('Introduce el nombre del festival a crear: ')
+            while True:
+                try:
+                    mes_fest = int(input('Introduce el mes del festival: '))
+                    if 0 < mes_fest < 13:
+                        break
+                    else:
+                        print('Número fuera del rango de meses, elija un mes del 1 al 12')
+                except ValueError:
+                    print('Por favor, introduzca un número válido')
+            while True:
+                try:
+                    anyo_fest = int(input('Introduce el año del festival: '))
+                    if 2024 < anyo_fest:
+                        break
+                    else:
+                        print('Número fuera del rango de años, elija un año del 2025 en adelante')
+                except ValueError:
+                    print('Por favor, introduzca un número válido')
+            while True:
+                try:
+                    dia_fest = int(input('Introduce el dia del festival: '))
+                    dias_mes= calendar.monthrange(anyo_fest, mes_fest)[1]
+                    if 0 < dia_fest <= dias_mes:
+                        break
+                    else:
+                        print(f'Número fuera del rango de dias, elija un dia del 1 al {dias_mes}')
+                except ValueError:
+                    print('Por favor, introduzca un número válido')
+            fecha_fest = f'{dia_fest}/{mes_fest}/{anyo_fest}'
+            lugar_fest = input('Introduce el lugar del festival: ')
+            aforo_fest = input('Introduce el aforo del festival a crear: ')
+            while not aforo_fest.isdigit():
+                print('Por favor, introduzca un número entero')
+                aforo_fest = input('Introduce el aforo del festival a crear: ')
+            aforo_fest = int(aforo_fest)
+            coste_fest = ''
+            while True:
+                try:
+                    coste_fest = float(input('Introduce el coste del festival (en €): '))
+                    break
+                except ValueError:
+                    print('Por favor, introduzca un número válido')
+            try:
+                r = requests.post(f'{URL}/data?nombre={nombre_fest}&fecha={fecha_fest}&lugar={lugar_fest}&aforo={aforo_fest}&coste={coste_fest}', headers={'Authorization': f'Bearer {token}'})
+                print(r.text + ' (' + str(r.status_code) + ')')
+                if r.status_code == 200:
+                    nuevo_festival = Festival(nombre_fest, fecha_fest, lugar_fest, aforo_fest, coste_fest)
+                    festivales.append(nuevo_festival)
+            except NameError:
+                print('No has iniciado sesión, no puedes realizar esta función. \n----------------------------------------1')
+
+
+
+        case '5':
+            if len(festivales)==0:
+                print("No hay festivales disponibles. Carga o crea alguno primero.")
+            else:
+                print('Elige un festival para eliminar:')
+                festival_a_eliminar = seleccionar_festival()
+                try:
+                    r = requests.delete(f'{URL}/data?festival={festival_a_eliminar.nombre}',headers={'Authorization': f'Bearer {token}'})
+                    print(r.text + ' (' + str(r.status_code) + ')')
+                    if r.status_code == 200:
+                        festivales.remove(festival_a_eliminar)
+                except NameError:
+                    print('No has iniciado sesión, no puedes realizar esta función. \n----------------------------------------1')
+
+
+        case '6':
+            if len(festivales) == 0:
+                print('No hay festivales disponibles')
+            else:
+                festival = seleccionar_festival()
+                opcion_servicio=''
+                opciones_mod=['1','2']
+                while opcion_servicio not in opciones_mod:
+                    print('1. Añadir servicio: ')
+                    print('2. Añadir artista: ')
+                    opcion_servicio = input('Introduce una de las opciones (por número): ')
+
+                if opcion_servicio == '1':
+                    opcion_puesto = ''
+                    opciones_puestos = ['1', '2', '3', '4', '5', '6']
+                    while opcion_puesto not in opciones_puestos:
+                        print('1. Puesto de Comida')
+                        print('2. Puesto de Bebidas')
+                        print('3. Puesto de de Tatuajes')
+                        print('4. Puesto de Merchandising')
+                        print('5. Servicio de Luces y Sonido')
+                        print('6. Servicio de Seguridad de las entradas')
+                        opcion_puesto = input('Introduce una de las opciones (por número): ')
+
+                    nombre = input('Introduce el nombre del servicio: ')
+                    horario = input('Introduce el horario del servicio: ')
+                    alquiler = float(input('Introduce el costo del alquiler del servicio: '))
+                    lugar  = input('Introduce el lugar donde se encuentra el puesto: ')
+
+                    if opcion_puesto == '1':
+                        comida1 = Comida(nombre, horario, alquiler, lugar, [])
+                        festival.anyadir_servicio(comida1)
+
+                    elif opcion_puesto == '2':
+                        bebida1 = Bebida(nombre, horario, alquiler, lugar, [])
+                        festival.anyadir_servicio(bebida1)
+
+                    elif opcion_puesto == '3':
+                        tattoo1 = Tattoo(nombre, horario, alquiler, lugar, [])
+                        festival.anyadir_servicio(tattoo1)
+
+                    elif opcion_puesto == '4':
+                        merch1 = Merch(nombre, horario, alquiler, lugar, [])
+                        festival.anyadir_servicio(merch1)
+
+                    elif opcion_puesto == '5':
+                        sonidoLuces1 = SonidoLuces(nombre, horario, alquiler, lugar, [])
+                        festival.anyadir_servicio(sonidoLuces1)
+
+                    elif opcion_puesto == '6':
+                        entradasSeguridad1 = EntradasSeguridad(nombre, horario, alquiler, lugar, [])
+                        festival.anyadir_servicio(entradasSeguridad1)
+
+                    r = requests.put(f'{URL}/data?nombre={festival}&servicio={nombre}')
+                    print(f'{r.text} ({r.status_code})')
+
+                elif opcion_servicio == '2':
+                    servicio_introducir_trabjador = seleccionar_servicio_de_festival(festival)
+
+                    fecha_nacimiento = input('Introduce tu fecha de nacimiento: ')
+                    dni = input('Introduce tu dni: ')
+                    nombre = input('Introduce tu nombre: ')
+                    apellido1 = input('Introduce tu primer apellido: ')
+                    apellido2 = input('Introduce tu segundo apellido: ')
+                    sueldo = float(input('Introduce el sueldo: '))
+
+                    trabajador = Staff(fecha_nacimiento, dni, nombre, apellido1, sueldo, servicio_introducir_trabjador.horario, '' , apellido2)
+                    servicio_introducir_trabjador.contratar_trabajador(trabajador)
+                    r = requests.put(f'{URL}/data?nombre={festival}&artista={nombre}')
+                    print(f'{r.text} ({r.status_code})')
+
+        case '7':
+            r = requests.get(f'{URL}/data_nombres')
+            if r.status_code == 200:
+                nombres = r.text.strip("[]").replace("'", "").split(", ")
+                print('Festivales registrados:')
+                for nombre in nombres:
+                    print(f"- {nombre}")
+            else:
+                print(f'Error al obtener festivales: {r.status_code} - {r.text}')
+
+        case '8':
             festival = seleccionar_festival()
-            opcion_servicio=''
-            opciones_mod=['1','2']
-            while opcion_servicio not in opciones_mod:
-                print('1. Añadir servicio: ')
-                print('2. Añadir artista: ')
-                opcion_servicio = input('Introduce una de las opciones (por número): ')
-
-            if opcion_servicio == '1':
-                opcion_puesto = ''
-                opciones_puestos = ['1', '2', '3', '4', '5', '6']
-                while opcion_puesto not in opciones_puestos:
-                    print('1. Puesto de Comida')
-                    print('2. Puesto de Bebidas')
-                    print('3. Puesto de de Tatuajes')
-                    print('4. Puesto de Merchandising')
-                    print('5. Servicio de Luces y Sonido')
-                    print('6. Servicio de Seguridad de las entradas')
-                    opcion_puesto = input('Introduce una de las opciones (por número): ')
-
-                nombre = input('Introduce el nombre del servicio: ')
-                horario = input('Introduce el horario del servicio: ')
-                alquiler = float(input('Introduce el costo del alquiler del servicio: '))
-                lugar  = input('Introduce el lugar donde se encuentra el puesto: ')
-
-                if opcion_puesto == '1':
-                    comida1 = Comida(nombre, horario, alquiler, lugar, [])
-                    festival.anyadir_servicio(comida1)
-
-                elif opcion_puesto == '2':
-                    bebida1 = Bebida(nombre, horario, alquiler, lugar, [])
-                    festival.anyadir_servicio(bebida1)
-
-                elif opcion_puesto == '3':
-                    tattoo1 = Tattoo(nombre, horario, alquiler, lugar, [])
-                    festival.anyadir_servicio(tattoo1)
-
-                elif opcion_puesto == '4':
-                    merch1 = Merch(nombre, horario, alquiler, lugar, [])
-                    festival.anyadir_servicio(merch1)
-
-                elif opcion_puesto == '5':
-                    sonidoLuces1 = SonidoLuces(nombre, horario, alquiler, lugar, [])
-                    festival.anyadir_servicio(sonidoLuces1)
-
-                elif opcion_puesto == '6':
-                    entradasSeguridad1 = EntradasSeguridad(nombre, horario, alquiler, lugar, [])
-                    festival.anyadir_servicio(entradasSeguridad1)
-
-                r = requests.put(f'{URL}/data?nombre={festival}&servicio={nombre}')
-                print(f'{r.text} ({r.status_code})')
-
-            elif opcion_servicio == '2':
-                servicio_introducir_trabjador = seleccionar_servicio_de_festival(festival)
-
-                fecha_nacimiento = input('Introduce tu fecha de nacimiento: ')
-                dni = input('Introduce tu dni: ')
-                nombre = input('Introduce tu nombre: ')
-                apellido1 = input('Introduce tu primer apellido: ')
-                apellido2 = input('Introduce tu segundo apellido: ')
-                sueldo = float(input('Introduce el sueldo: '))
-
-                trabajador = Staff(fecha_nacimiento, dni, nombre, apellido1, sueldo, servicio_introducir_trabjador.horario, '' , apellido2)
-                servicio_introducir_trabjador.contratar_trabajador(trabajador)
-                r = requests.put(f'{URL}/data?nombre={festival}&artista={nombre}')
-                print(f'{r.text} ({r.status_code})')
-
-    elif opcion == '7':
-        r = requests.get(f'{URL}/data_nombres')
-        if r.status_code == 200:
-            nombres = r.text.strip("[]").replace("'", "").split(", ")
-            print('Festivales registrados:')
-            for nombre in nombres:
-                print(f"- {nombre}")
-        else:
-            print(f'Error al obtener festivales: {r.status_code} - {r.text}')
-
-    elif opcion == '8':
-        festival = seleccionar_festival()
-        print(festival)
-    elif opcion == '9':
-        festival = seleccionar_festival()
-        servicio = seleccionar_servicio_de_festival(festival)
-        print(servicio)
-    elif opcion == '10':
-        festival = seleccionar_festival()
-        servicio = seleccionar_servicio_de_festival(festival)
-        trabajador = seleccionar_trabajador_de_servicio(servicio)
-        print(trabajador)
-    elif opcion == '11':
+            print(festival)
+        case '9':
+            festival = seleccionar_festival()
+            servicio = seleccionar_servicio_de_festival(festival)
+            print(servicio)
+        case '10':
+            festival = seleccionar_festival()
+            servicio = seleccionar_servicio_de_festival(festival)
+            trabajador = seleccionar_trabajador_de_servicio(servicio)
+            print(trabajador)
+        case '11':
 
 
-        pass
-    elif opcion == '12':
-        r = requests.get(f'{URL}/data/publico')
-        pass
-    elif opcion == '13':
-        print('Cerrando programa)')
-        break
+            pass
+        case '12':
+            r = requests.get(f'{URL}/data/publico')
+            pass
+        case '13':
+            print('Cerrando programa)')
+            break
